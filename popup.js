55a62efff2aaa0c83890666e8ed42feb9f2b3156
Vidal Graupera <vgraupera@gmail.com>
2023-08-19 07:57:52 -0700

    basic redirect working

--- popup.js
+++ popup.js
@@ -1,21 +1,22 @@
 function onWindowLoad() {
-    const scrapeBtn = document.getElementById("scrapeBtn");
-    const scrapedText = document.getElementById("scrapedText");
+  const scrapeBtn = document.getElementById("scrapeBtn");
 
-    scrapeBtn.onclick = function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                files: ['content.js']
-            });
-        });        
-    };
+  scrapeBtn.onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["content.js"],
+      });
+    });
+  };
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action == "getSource") {
-        document.getElementById("scrapedText").innerText = request.source;
+      const encodedText = encodeURIComponent(request.source);
+      const url = `https://interviewigniter.com/job-description?text=${encodedText}`;
+      chrome.tabs.create({ url });
     }
-});
+  });  
 
 window.onload = onWindowLoad;
